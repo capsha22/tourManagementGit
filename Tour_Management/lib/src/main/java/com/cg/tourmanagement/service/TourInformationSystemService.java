c993745ed14437994b0bf4984948b27a1a4d1877
capsha22 <shashank.b.sharma@capgemini.com>
2022-05-27 16:56:28 +0530

    by sha

--- Tour_Management/lib/src/main/java/com/cg/tourmanagement/service/TourInformationSystemService.java
+++ Tour_Management/lib/src/main/java/com/cg/tourmanagement/service/TourInformationSystemService.java
@@ -1,8 +1,17 @@
 package com.cg.tourmanagement.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.cg.tourmanagement.entities.TourInformationSystem;
+import com.cg.tourmanagement.exception.TourInformationSystemAlreadyExistException;
+import com.cg.tourmanagement.exception.TourInformationSystemNotFoundException;
 
 public interface TourInformationSystemService {
-	void addPackage (TourInformationSystem tour);
+	void addPackage (TourInformationSystem tour) throws TourInformationSystemAlreadyExistException;
+	List<TourInformationSystem> viewAllTours();
+	Optional<TourInformationSystem> getTourById(int packageId) throws TourInformationSystemNotFoundException;
+	void updateDepartment(TourInformationSystem tour);
+	void deleteDepartment(int deptno);
 
 }
