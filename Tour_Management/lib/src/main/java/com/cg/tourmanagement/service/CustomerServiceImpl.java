f405d24cc4dd87a968c999fbbf7d1da6f73af0dd
mlk912 <42536401+mallikarjun912@users.noreply.github.com>
2022-05-27 14:55:19 +0530

    mlk- CustomerService - updated

--- Tour_Management/lib/src/main/java/com/cg/tourmanagement/service/CustomerServiceImpl.java
+++ Tour_Management/lib/src/main/java/com/cg/tourmanagement/service/CustomerServiceImpl.java
@@ -1,5 +1,52 @@
 package com.cg.tourmanagement.service;
 
-public class CustomerServiceImpl {
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.cg.tourmanagement.Exception.CustomerPasswordException;
+import com.cg.tourmanagement.Exception.CustomerUserIdAlreadyExistsException;
+import com.cg.tourmanagement.entities.Customer;
+import com.cg.tourmanagement.entities.TourInfo;
+import com.cg.tourmanagement.entities.TourInformationSystem;
+
+@Service
+public class CustomerServiceImpl implements CustomerService{
+
+	@Override
+	public void addCustomer(Customer customer) throws CustomerUserIdAlreadyExistsException, CustomerPasswordException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Optional<TourInformationSystem> getpackageName(String packageName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void AddPackage(TourInfo packagename) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateTourInfo(TourInfo tourinfo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Optional<TourInfo> reserevdPackageId(int reserevdPackageId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void cancelRegistration(int customerId) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
