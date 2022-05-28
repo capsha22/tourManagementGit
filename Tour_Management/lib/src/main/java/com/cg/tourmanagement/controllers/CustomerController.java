c89acdab5bb2105e3ecb8ae14bbed0fee60f75af
AkshayV9 <103674117+AkshayV9@users.noreply.github.com>
2022-05-28 17:19:59 +0530

    by akshay

--- Tour_Management/lib/src/main/java/com/cg/tourmanagement/controllers/CustomerController.java
+++ Tour_Management/lib/src/main/java/com/cg/tourmanagement/controllers/CustomerController.java
@@ -3,31 +3,15 @@ package com.cg.tourmanagement.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-=======
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 import com.cg.tourmanagement.service.CustomerServiceImpl;
-
-public class CustomerController  {
-	@Autowired
-	CustomerServiceImpl service;
-	
-	
-	@DeleteMapping("/id/{customerid}")
-	public ResponseEntity<String> addPackage(@RequestBody int customerId){
-		service.cancelRegistration(customerId);
-		return new ResponseEntity<String>("deleted", HttpStatus.OK);
-
->>>>>>> ff68cb3da84262f377a80b91e87d1269845e88c4
 
 import com.cg.tourmanagement.dto.CustomerDto;
-import com.cg.tourmanagement.service.CustomerServiceImpl;
+import com.cg.tourmanagement.dto.TourInfoDto;
 
 @RestController
 @RequestMapping("/cust")
@@ -35,6 +19,17 @@ public class CustomerController {
 	@Autowired
     CustomerServiceImpl custservice;
 	
+	@PostMapping
+	public ResponseEntity<String>addPackage(@RequestBody TourInfoDto tourdto){
+	custservice.AddPackage(tourdto);
+		return new ResponseEntity<String>("inserted", HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/id/{customerid}")
+	public ResponseEntity<String>cancelRegistration(@RequestBody int customerId){
+	custservice.cancelRegistration(customerId);
+		return new ResponseEntity<String>("deleted", HttpStatus.OK);
+	}
 	
 	@PostMapping
 	public ResponseEntity<String> addCustomer(@RequestBody CustomerDto custdto){
@@ -43,5 +38,4 @@ public class CustomerController {
 	}
 	
 	
-}
 }
